f6e85f0efef524280b65be700472c504c4949b6c
Kristopher <root@sandb0x.io>
2023-09-16 15:51:49 +0800

    removed submission time offset

--- components/Submission.tsx
+++ components/Submission.tsx
@@ -15,7 +15,6 @@ export function Submission({ submission }) {
   }
 
   const submittedDate = new Date(submission.created_at);
-  submittedDate.setTime(submittedDate.getTime()+8*60*60*1000);
   return (
     <li>
       <ul>
